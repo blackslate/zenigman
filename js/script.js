b617f6df1cf8687d99e30f3e3b15d9e29b5b331f
James Newton <james.lexogram@gmail.com>
2019-08-17 16:09:54 +0300

    Add simple multilingual interface
    - Create <jx-tr> element to show translation
    - Add Google translation of first page by hand
    - Add <aside> to show choice of vo and translation
    - JS and CSS for showing and hiding translation and <aside>

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /** script.js **
  *
- * 
+ *
 **/
 
 
@@ -21,12 +21,190 @@
 
 
 
+  ;(function createJXTRElement() {
+
+    // https://developers.google.com/web/fundamentals/web-components/examples/howto-checkbox
+
+    const template = document.createElement('template')
+
+    template.innerHTML = `
+      <style>
+        :host {
+          display: inline-block;
+          background: #900;
+          background-size: contain;
+          width: 1em;
+          height: 1em;
+        }
+        :host([hidden]) {
+          display: none;
+        }
+        :host([checked]) {
+          background: #090;
+          background-size: contain;
+        }
+        :host([disabled]) {
+          background: #966;
+          background-size: contain;
+        }
+        :host([checked][disabled]) {
+          background: #696;
+          background-size: contain;
+        }
+      </style>
+    `
+
+      class VO2 extends HTMLElement {
+
+        static get observedAttributes() {
+          return ['checked', 'disabled'];
+        }
+
+
+        constructor() {
+          super()
+          this.attachShadow({mode: 'open'})
+          this.shadowRoot.appendChild(template.content.cloneNode(true))
+        }
+
+
+        connectedCallback() {
+          if (!this.hasAttribute('role')) {
+            this.setAttribute('role', 'checkbox');
+          }
+          if (!this.hasAttribute('tabindex')) {
+            this.setAttribute('tabindex', 0);
+          }
+
+          this._upgradeProperty('checked');
+          this._upgradeProperty('disabled');
+
+          this.addEventListener('keyup', this._onKeyUp);
+          this.addEventListener('click', this._onClick);
+        }
+
+
+        disconnectedCallback() {
+          this.removeEventListener('keyup', this._onKeyUp);
+          this.removeEventListener('click', this._onClick);
+        }
+
+
+        _upgradeProperty(prop) {
+          if (this.hasOwnProperty(prop)) {
+            let value = this[prop];
+            delete this[prop];
+            this[prop] = value;
+          }
+        }
+
+
+        set checked(value) {
+          const isChecked = Boolean(value);
+          if (isChecked) {
+            this.setAttribute('checked', '');
+          } else {
+            this.removeAttribute('checked');
+          }
+        }
+
+
+        get checked() {
+          return this.hasAttribute('checked');
+        }
+
+
+        set disabled(value) {
+          const isDisabled = Boolean(value);
+
+          if (isDisabled) {
+            this.setAttribute('disabled', '');
+          } else {
+            this.removeAttribute('disabled');
+          }
+        }
+
+
+        get disabled() {
+          return this.hasAttribute('disabled');
+        }
+
+
+        attributeChangedCallback(name, oldValue, newValue) {
+          const hasValue = newValue !== null;
+
+          switch (name) {
+            case 'checked':
+              this.setAttribute('aria-checked', hasValue);
+            break;
+
+            case 'disabled':
+              this.setAttribute('aria-disabled', hasValue);
+              if (hasValue) {
+                this.removeAttribute('tabindex');
+                this.blur();
+
+              } else {
+                this.setAttribute('tabindex', '0');
+              }
+            break;
+          }
+        }
+
+
+        _onKeyUp(event) {
+          if (event.altKey) { return }
+
+          switch (event.keyCode) {
+            case KEYCODE.SPACE:
+              event.preventDefault();
+              this._toggleChecked();
+            break;
+            default:
+              return;
+          }
+        }
+
+
+        _onClick(event) {
+          this._toggleChecked();
+        }
+
+
+        _toggleChecked() {
+          if (this.disabled) { return }
+
+          this.checked = !this.checked;
+
+          this.dispatchEvent (
+            new CustomEvent('change'
+          , { detail:
+              { checked: this.checked }
+            , bubbles: true
+            }
+          ));
+        }
+      }
+
+
+    window.customElements.define("jx-tr", VO2)
+  })()
+
+
+
   jazyx.classes.Script = class Script {
     constructor() {
       this.divs = [].slice.apply(document.querySelectorAll("div"))
+      this.langButtons = this._getElements("aside input")
 
       let listener = this.treatClick.bind(this)
       document.body.onclick = listener
+
+      listener = this.setVO.bind(this)
+      let aside = document.querySelector("aside")
+      aside.onchange = listener
+
+      listener()
 
       listener = this.scroll.bind(this)
       document.body.scroll = listener
@@ -47,7 +225,7 @@
       if (target.tagName !== "A") {
         return
       }
-      
+
       let divId = target.href.split("#").pop()
 
 
@@ -75,6 +253,40 @@
       let scrollBottom = this.height - scrollTop
       let fullScroll = !(scrollBottom > window.innerHeight)
       this._showFullScrollState(fullScroll)
+    }
+
+
+    setVO(event) {
+      let htmlElement = document.querySelector("html")
+      let target
+        , id
+        , input
+
+      if (event) {
+        target = event.target
+        if (target.tagName !== "INPUT") { return }
+
+        id = target.id
+        htmlElement.setAttribute("lang", id)
+
+      } else {
+        id = htmlElement.getAttribute("lang") || "en"
+        this.langButtons[id].checked = true
+      }
+
+      console.log(htmlElement.getAttribute("lang"))
+    }
+
+
+    _getElements(selector) {
+      let elements = {}
+      let array = [].slice.call(document.querySelectorAll(selector))
+
+      array.forEach(element => {
+        elements[element.id] = element
+      })
+
+      return elements
     }
 
 
